f9639f53a464cd81f453085506d97ce5ca43d94b
s344051 <s344051@oslomet.no>
2020-10-22 23:19:40 +0200

    Oppgave 4: postordenRecursive() er ferdig. Oppgave 4 ferdig.

--- src/no/oslomet/cs/algdat/Eksamen/EksamenSBinTre.java
+++ src/no/oslomet/cs/algdat/Eksamen/EksamenSBinTre.java
@@ -196,7 +196,12 @@ public class EksamenSBinTre<T> {
     }
 
     private void postordenRecursive(Node<T> p, Oppgave<? super T> oppgave) {
-        throw new UnsupportedOperationException("Ikke kodet ennå!");
+
+        if (p != null){
+            postordenRecursive(p.venstre, oppgave);
+            postordenRecursive(p.høyre, oppgave);
+            oppgave.utførOppgave(p.verdi);
+        }
     }
 
     public ArrayList<T> serialize() {
